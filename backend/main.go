21ba4ea61494dde2904c84f25ff336de73aae87a
phurba-sherpa <phurba1404@gmail.com>
2025-02-21 21:14:03 +0545

    feat(core): setup entry file for server start, db con and migration

--- backend/main.go
+++ backend/main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"github.com/phurba-sherpa/task-management-backend/db"
+	"github.com/phurba-sherpa/task-management-backend/models"
+	"github.com/phurba-sherpa/task-management-backend/routes"
 	"github.com/rs/zerolog/log"
 )
 
-func main () {
-    log.Info().Msg("Starting server...")
-    db.InitDB()
+func main() {
+	log.Info().Msg("Starting server...")
+	db.InitDB()
+
+	log.Info().Msg("Running migrations...")
+	db.DB.AutoMigrate(&models.Task{})
+	log.Info().Msg("Migrations complted!")
+
+	r := routes.SetupRoutes()
+	log.Info().Msg("Server started successfully!")
+	r.Run()
+
 }
