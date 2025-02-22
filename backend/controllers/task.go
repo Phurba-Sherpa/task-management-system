1c539e8f531de7e95d580495380216b9552899e7
phurba-sherpa <phurba1404@gmail.com>
2025-02-22 08:12:45 +0545

    chore(http-status): use status code provided by http

--- backend/controllers/task.go
+++ backend/controllers/task.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	apihandlers "github.com/phurba-sherpa/task-management-backend/api-handlers"
 	"github.com/phurba-sherpa/task-management-backend/models"
@@ -12,23 +14,24 @@ func GetAllTasks(c *gin.Context) {
 	err := models.GetAllTasks(&tasks)
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to fetch tasks")
-		apihandlers.RespondJSON(c, 404, tasks, "Failed to fetch tasks, something went wrong!")
+		apihandlers.RespondJSON(c, http.StatusInternalServerError, tasks, "Failed to fetch tasks, something went wrong!")
 	} else {
 		log.Info().Interface("tasks", tasks).Msg("Fetched tasks")
-		apihandlers.RespondJSON(c, 200, tasks, "Records fetched successfully!")
+		apihandlers.RespondJSON(c, http.StatusOK, tasks, "Records fetched successfully!")
 	}
 }
 
 func AddNewTask(c *gin.Context) {
 	var task models.Task
+
 	c.BindJSON(&task)
 	err := models.AddNewTask(&task)
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to add task")
-		apihandlers.RespondJSON(c, 400, task, "Failed to save task!")
+		apihandlers.RespondJSON(c, http.StatusBadRequest, task, "Failed to save task!")
 	} else {
 		log.Info().Interface("task", task).Msg("Successfully saved task")
-		apihandlers.RespondJSON(c, 201, task, "Task saved successfully!")
+		apihandlers.RespondJSON(c, http.StatusCreated, task, "Task saved successfully!")
 	}
 }
 
@@ -38,10 +41,10 @@ func GetTaskById(c *gin.Context) {
 	err := models.GetTaskById(&task, id)
 	if err != nil {
 		log.Error().Err(err).Str("task_id", id).Msg("Failed to fetch task")
-		apihandlers.RespondJSON(c, 404, nil, "Task not found!")
+		apihandlers.RespondJSON(c, http.StatusNotFound, nil, "Task not found!")
 	} else {
 		log.Info().Interface("tasks", task).Msg("Fetched task")
-		apihandlers.RespondJSON(c, 200, task, "Task successfully fetched!")
+		apihandlers.RespondJSON(c, http.StatusOK, task, "Task successfully fetched!")
 	}
 }
 
@@ -50,14 +53,14 @@ func UpdateTask(c *gin.Context) {
 	id := c.Params.ByName("id")
 	err := models.GetTaskById(&task, id)
 	if err != nil {
-		apihandlers.RespondJSON(c, 404, task, "Task doesnt exist!")
+		apihandlers.RespondJSON(c, http.StatusNotFound, task, "Task doesnt exist!")
 	}
 	c.BindJSON(&task)
 	err = models.UpdateTask(&task, id)
 	if err != nil {
-		apihandlers.RespondJSON(c, 400, task, "Task update failed!")
+		apihandlers.RespondJSON(c, http.StatusBadRequest, task, "Task update failed!")
 	} else {
-		apihandlers.RespondJSON(c, 200, task, "Task successfully updated!")
+		apihandlers.RespondJSON(c, http.StatusOK, task, "Task successfully updated!")
 	}
 }
 
@@ -67,8 +70,8 @@ func DeleteTask(c *gin.Context) {
 
 	err := models.DeleteTask(&task, id)
 	if err != nil {
-		apihandlers.RespondJSON(c, 400, task, "Task deletion failed")
+		apihandlers.RespondJSON(c, http.StatusBadRequest, task, "Task deletion failed")
 	} else {
-		apihandlers.RespondJSON(c, 200, task, "Task successfully deleted")
+		apihandlers.RespondJSON(c, http.StatusOK, task, "Task successfully deleted")
 	}
 }
