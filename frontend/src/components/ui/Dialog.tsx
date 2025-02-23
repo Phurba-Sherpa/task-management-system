15d0ae959274be6482c6309040f008eda3ffdbca
phurba-sherpa <phurba1404@gmail.com>
2025-02-23 12:29:30 +0545

    feat(dialog): add dialog component

---
+++ frontend/src/components/ui/Dialog.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { Dialog, DialogContent, DialogTitle } from "@mui/material";
+
+type DialogProps = {
+  open: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+  title: string;
+  maxWidth: "lg" | "md" | "sm";
+};
+const DialogWrapper: React.FC<DialogProps> = ({
+  maxWidth = "lg",
+  open,
+  onClose,
+  children,
+  title,
+}) => {
+  return (
+    <Dialog
+      fullWidth={true}
+      maxWidth={maxWidth}
+      open={open}
+      onClose={onClose}
+      sx={{
+        "& .MuiDialogTitle-root": {
+          px: 4,
+          py: 3,
+        },
+        "& .MuiDialogContent-root": {
+          padding: 4,
+        },
+      }}
+    >
+      <DialogTitle sx={{ fontWeight: 500, fontSize: "24px" }}>
+        {title}
+      </DialogTitle>
+      <DialogContent dividers>{children}</DialogContent>
+    </Dialog>
+  );
+};
+
+export default React.memo(DialogWrapper);
