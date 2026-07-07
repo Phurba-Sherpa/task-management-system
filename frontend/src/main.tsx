ff2ed9cdcb915f6d545d6a8eb170e7ab43ee2739
phurba-sherpa <phurba1404@gmail.com>
2026-07-07 08:26:07 +0545

    chore: retrigger

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import "./index.css";
 import App from "./app";
+
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")!).render(
